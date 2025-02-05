e0dbe157de379aab28de81c9f5ab42c0779776b9
John <jbotonakis@carroll.edu>
2025-02-05 12:57:00 -0700

    Added a nav bar, but something goofy going on with "Hello World"

--- src/main/kotlin/com/example/kotapp/controllers/HelloController.kt
+++ src/main/kotlin/com/example/kotapp/controllers/HelloController.kt
@@ -1,12 +1,7 @@
 package com.example.kotap.controllers
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.ResponseBody
-import java.nio.file.Files
-import java.nio.file.Paths
 
 /**
  * Controller responsible for serving the main HTML page.
@@ -14,7 +9,6 @@ import java.nio.file.Paths
  * web requests
  */
 @Controller //Controllers handle web requests
-@RequestMapping("/")// Maps this controller to handler requests at the root URL which is '/'
 class HelloController {
     /**
      * Handles GET requests to the root URL '/'
@@ -22,10 +16,8 @@ class HelloController {
      *
      * @return The HTML content of the file
      */
-    @GetMapping // GetMapping maps the root URL, this case it's "/", to serve as the HTML file
-    @ResponseBody  //Returns the raw HTML content instead of a view
-    fun serveHtml(): String {
-        val path = Paths.get("src/main/resources/static/index.html")//Path to the HTML file
-        return Files.readString(path) // Reads the file contents and returns them as a string
+    @GetMapping("/") // GetMapping maps the root URL, this case it's "/", to serve as the HTML file
+    fun home(): String {
+        return "index.html" // Reads the file contents and returns them as a string
     }
 }
